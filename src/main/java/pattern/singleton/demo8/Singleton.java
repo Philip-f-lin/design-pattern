c48820054d10fbb68b7fdbc9e29023279d3846ea
linzhuofei <philip.f.lin@gmail.com>
2025-03-11 17:02:35 +0800

    設計模式 - 創建者模式 - 單例設計模式 - 反射破壞單例模式的解決方案

--- src/main/java/pattern/singleton/demo8/Singleton.java
+++ src/main/java/pattern/singleton/demo8/Singleton.java
@@ -3,8 +3,20 @@ package pattern.singleton.demo8;
 import java.io.Serializable;
 
 public class Singleton implements Serializable {
+
+    private static boolean flag = false;
+
     // 私有構造函數
     private Singleton() {
+        synchronized (Singleton.class) {
+            // 判斷 flag 的值，是否是 true，如果是 true，說明不是第一次訪問，直接拋一個異常
+            // 如果是 false，說明是第一次訪問
+            if (flag) {
+                throw new RuntimeException("不能創建多個物件");
+            }
+            // 將 flag 設置為 true
+            flag = true;
+        }
     }
 
     // 定義一個靜態內部類
